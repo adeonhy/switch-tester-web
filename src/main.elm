607da12ff03688c387a786237143936794ae1ebc
Naoya Matsuyama <adeonhy@gmail.com>
2019-04-02 17:23:32 +0900

    implement first elm version.

--- src/main.elm
+++ src/main.elm
@@ -2,8 +2,13 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Browser.Events exposing (onKeyDown)
-import Html exposing (Html, button, div, text)
-import Json.Decode as Decode
+import Debug
+import Dict exposing (Dict)
+import Dict.Extra
+import Html exposing (Html, dd, div, dl, dt, h2, img, p, text)
+import Html.Attributes exposing (src, style)
+import Http
+import Json.Decode exposing (Decoder, at, field, int, list, map3, string)
 import Time
 
 
@@ -11,47 +16,8 @@ claerInterval =
     5
 
 
-type alias KeySwitch =
-    { brand : SwitchBrand
-    , switchType : SwitchType
-    , series : Maybe String
-    , name : String
-    , silent : Bool
-    , price : Int
-    }
-
-
-type SwitchBrand
-    = Gateron
-    | Kailh
-    | Outemu
-    | Cherry
-
-
-type SwitchType
-    = Linear
-    | Tactile
-    | Clicky
-
-
-switch : SwitchBrand -> SwitchType -> String -> String -> Bool -> Int -> KeySwitch
-switch brand sType series name slient price =
-    { brand = brand
-    , switchType = sType
-    , series = series
-    , name = name
-    , silent = silent
-    , price = price
-    }
-
-
-keyMappings : { key : Char, switch : KeySwitch }
-keyMappings =
-    [ { key = 'a', switch = switch Gateron Linear Nothing "Red" false 48 }
-    , { key = 'b', switch = switch Gateron Tactile Nothing "Brown" false 48 }
-    , { key = 'c', switch = switch Gateron Clicky Nothing "Blue" false 48 }
-    , { key = 'd', switch = switch Gateron Linear Nothing "Clear" false 48 }
-    ]
+urlOfSwitchesSpreadsheetJson =
+    "https://spreadsheets.google.com/feeds/cells/1G0grm4xDFYO-9X4sWX7A8I2V7ag2H9fG2zTa2dnkgow/od6/public/values?alt=json"
 
 
 main =
@@ -71,13 +37,23 @@ type alias Model =
     { count : Int
     , key : String
     , modifier : String
+    , status : Status
     }
+
+
+type Status
+    = Initial
+    | Loading
+    | Tester (List KeyMapping)
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { count = 0, key = "", modifier = "" }
-    , Cmd.none
+    ( { count = 0, key = "", modifier = "", status = Loading }
+    , Http.get
+        { url = urlOfSwitchesSpreadsheetJson
+        , expect = Http.expectJson GotResponse switchJsonDecoder
+        }
     )
 
 
@@ -88,11 +64,21 @@ init _ =
 type Msg
     = Tick Time.Posix
     | Input Key
+    | GotResponse (Result Http.Error (List Cell))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        GotResponse result ->
+            case result of
+                Ok cells ->
+                    ( { model | status = Tester (toKeyMappings cells) }, Cmd.none )
+
+                -- ( { model | modifier = Debug.toString (toKeyMappings cells) }, Cmd.none )
+                Err _ ->
+                    ( { model | modifier = "error" }, Cmd.none )
+
         Tick _ ->
             if model.count >= claerInterval then
                 ( { model | count = 0, key = "", modifier = "" }, Cmd.none )
@@ -101,7 +87,7 @@ update msg model =
                 ( { model | count = model.count + 1 }, Cmd.none )
 
         Input (Character c) ->
-            ( { model | key = String.fromChar c }, Cmd.none )
+            ( { model | count = 0, key = String.fromChar c }, Cmd.none )
 
         Input (Control s) ->
             ( { model | modifier = s }, Cmd.none )
@@ -115,7 +101,7 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [ Time.every 1000 Tick
-        , onKeyDown (Decode.map Input keyDecoder)
+        , onKeyDown (Json.Decode.map Input keyDecoder)
         ]
 
 
@@ -125,11 +111,68 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    div [ style "text-align" "center" ]
+        [ viewSwitch (switchOf model)
+        , viewDebugInfo model
+        ]
+
+
+viewSwitch : Maybe KeyMapping -> Html Msg
+viewSwitch mapping =
+    case mapping of
+        Nothing ->
+            viewDefault
+
+        Just m ->
+            dl []
+                (List.append
+                    [ h2 [] [ text ("押したスイッチ: " ++ m.switch) ] ]
+                    (viewSwitchAttr m)
+                )
+
+
+viewSwitchAttr : KeyMapping -> List (Html Msg)
+viewSwitchAttr m =
+    Dict.toList m.attr
+        |> List.map
+            (\( k, v ) ->
+                [ p [] [ text (k ++ ": " ++ v) ]
+                ]
+            )
+        |> List.concat
+
+
+viewDefault : Html Msg
+viewDefault =
     div []
+        [ img
+            [ src "../assets/computer_blindtouch.png"
+            , style "width" "25%"
+            , style "height" "25%"
+            ]
+            []
+        , p []
+            [ text "スイッチを押してね！" ]
+        ]
+
+
+viewDebugInfo : Model -> Html Msg
+viewDebugInfo model =
+    div [ style "color" "white" ]
         [ div [] [ text (String.fromInt model.count) ]
         , div [] [ text model.key ]
         , div [] [ text model.modifier ]
         ]
+
+
+switchOf : Model -> Maybe KeyMapping
+switchOf model =
+    case model.status of
+        Tester mappings ->
+            List.head (List.filter (\k -> k.key == model.key) mappings)
+
+        _ ->
+            Nothing
 
 
 
@@ -141,9 +184,9 @@ type Key
     | Control String
 
 
-keyDecoder : Decode.Decoder Key
+keyDecoder : Json.Decode.Decoder Key
 keyDecoder =
-    Decode.map toKey (Decode.field "key" Decode.string)
+    Json.Decode.map toKey (Json.Decode.field "key" Json.Decode.string)
 
 
 toKey : String -> Key
@@ -154,3 +197,78 @@ toKey string =
 
         _ ->
             Control string
+
+
+
+-- Switch JSON DECODER
+
+
+switchJsonDecoder : Decoder (List Cell)
+switchJsonDecoder =
+    at [ "feed", "entry" ] (list entryDecoder)
+
+
+entryDecoder : Decoder Cell
+entryDecoder =
+    map3 Cell
+        (at [ "gs$cell", "row" ] string)
+        (at [ "gs$cell", "col" ] string)
+        (at [ "gs$cell", "$t" ] string)
+
+
+type alias Cell =
+    { row : String, col : String, value : String }
+
+
+type alias KeyMapping =
+    { key : String
+    , switch : String
+    , attr : Dict String String
+    , row : Int
+    }
+
+
+toKeyMappings : List Cell -> List KeyMapping
+toKeyMappings cells =
+    let
+        grouped : Dict String (List Cell)
+        grouped =
+            Dict.Extra.groupBy .row cells
+
+        headers =
+            Dict.get "1" grouped
+                |> Maybe.withDefault []
+                |> List.map (\c -> ( c.col, c.value ))
+                |> Dict.fromList
+
+        rows : List (List Cell)
+        rows =
+            Dict.values grouped
+
+        rowToRecord cols =
+            let
+                rowToRecord_ cols_ record =
+                    case cols_ of
+                        col :: rest ->
+                            case col.col of
+                                "1" ->
+                                    rowToRecord_ rest record
+
+                                "2" ->
+                                    rowToRecord_ rest record
+
+                                "3" ->
+                                    rowToRecord_ rest { record | key = col.value }
+
+                                "4" ->
+                                    rowToRecord_ rest { record | switch = col.value }
+
+                                _ ->
+                                    rowToRecord_ rest { record | attr = Dict.insert (Dict.get col.col headers |> Maybe.withDefault "") col.value record.attr }
+
+                        [] ->
+                            record
+            in
+            rowToRecord_ cols (KeyMapping "" "" Dict.empty 0)
+    in
+    List.map rowToRecord rows
