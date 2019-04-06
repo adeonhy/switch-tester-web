6f7f1e26c207f3c856d4bfd76d3b8ba552c189be
Naoya Matsuyama <adeonhy@gmail.com>
2019-04-06 13:29:51 +0900

    fix path

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -11744,7 +11744,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63105" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51536" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -11920,4 +11920,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../.nodebrew/node/v9.11.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
-//# sourceMappingURL=/js.00a46daa.js.map
+//# sourceMappingURL=js.00a46daa.js.map
